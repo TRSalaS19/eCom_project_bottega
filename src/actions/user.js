0d3579ac669746af853236096ca00ac00230358b
TRSalaS19 <gruiz0803@icloud.com>
2021-02-08 09:17:23 -0800

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,14 +8,92 @@ export function fetchUserPurchases() {
         payload: [
           {
             _id: 0,
-            title: 'purchase 1',
-            amount: 8.02
+            total: 8.02,
+            orderNumber: 'A0048248343',
+            orderDate: new Date(),
+            creditCard: '-0000',
+            user: {
+                name: 'Gustavo Salas',
+                shippingAddress: '1234 South Main street'
+            }
           },
           {
             _id: 1,
-            title: 'purchase 2',
-            amount: 18.02
-          }
+            total: 8.02,
+            orderNumber: 'A0048248344',
+            orderDate: new Date(),
+            creditCard: '-0000',
+            user: {
+                name: 'Antonio Sanchez',
+                shippingAddress: '5555 south 14th street'
+            }
+          },
+          {
+            _id: 2,
+            total: 18.52,
+            orderNumber: 'A0048248345',
+            orderDate: new Date(),
+            creditCard: '-0000',
+            user: {
+                name: 'Maria Hernandez',
+                shippingAddress: '200 South Main street'
+            }
+          },
+          {
+            _id: 3,
+            total: 4.02,
+            orderNumber: 'A0048248346',
+            orderDate: new Date(),
+            creditCard: '-0000',
+            user: {
+                name: 'Evelinda Ochoa',
+                shippingAddress: '2020 Humbolt Street'
+            }
+          },
+          {
+            _id: 4,
+            total: 3.58,
+            orderNumber: 'A0048248347',
+            orderDate: new Date(),
+            creditCard: '-0000',
+            user: {
+                name: 'Tina Salas',
+                shippingAddress: '400 1st Street'
+            }
+          },
+          {
+            _id: 5,
+            total: 38.21,
+            orderNumber: 'A0048248347',
+            orderDate: new Date(),
+            creditCard: '-0000',
+            user: {
+                name: 'Sammy Ruiz',
+                shippingAddress: '8576 Tree Street'
+            }
+          },
+          {
+            _id: 6,
+            total: 50.22,
+            orderNumber: 'A0048248348',
+            orderDate: new Date(),
+            creditCard: '-0000',
+            user: {
+                name: 'Ezekiel Ochoa',
+                shippingAddress: '200 Humbolt Street'
+            }
+          },
+          {
+            _id: 7,
+            total: 42.52,
+            orderNumber: 'A0048248349',
+            orderDate: new Date(),
+            creditCard: '-0000',
+            user: {
+                name: 'Trinity Ochoa',
+                shippingAddress: '42500 Humbolt Street'
+            }
+          },
         ]
     })
 }
